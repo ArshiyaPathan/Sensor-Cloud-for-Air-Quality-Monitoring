a83bc23437111eda15d3313a6777cb5165d0990f
VineelaChallagulla <vineelachallagulla@gmail.com>
2018-05-15 17:05:33 -0700

    Update main.js

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -271,13 +271,13 @@ $(document).ready(function () {
             }],
             yAxis: [{ // Primary yAxis
                 labels: {
-                    format: '{value}°C',
+                    format: '{value}ppb',
                     style: {
                         color: Highcharts.getOptions().colors[2]
                     }
                 },
                 title: {
-                    text: 'Carbon Dioxide',
+                    text: 'Ozone',
                     style: {
                         color: Highcharts.getOptions().colors[2]
                     }
@@ -293,7 +293,7 @@ $(document).ready(function () {
                     }
                 },
                 labels: {
-                    format: '{value} mm',
+                    format: '{value} ppb',
                     style: {
                         color: Highcharts.getOptions().colors[0]
                     }
@@ -308,7 +308,7 @@ $(document).ready(function () {
                     }
                 },
                 labels: {
-                    format: '{value} mb',
+                    format: '{value} ppb',
                     style: {
                         color: Highcharts.getOptions().colors[1]
                     }
@@ -333,7 +333,7 @@ $(document).ready(function () {
                 yAxis: 1,
                 data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
                 tooltip: {
-                    valueSuffix: ' mm'
+                    valueSuffix: ' ppb'
                 }
 
             }, {
@@ -350,11 +350,11 @@ $(document).ready(function () {
                 }
 
             }, {
-                name: 'Carbon Dioxide',
+                name: 'Ozone',
                 type: 'spline',
                 data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6],
                 tooltip: {
-                    valueSuffix: ' °C'
+                    valueSuffix: ' ppb'
                 }
             }]
         };
